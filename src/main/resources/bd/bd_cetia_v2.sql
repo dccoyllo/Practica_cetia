e30fd20e38a63d994ec6403f2d615b0dc084af80
Javier Yace Huaman <44007462+yaceh@users.noreply.github.com>
2020-10-29 15:43:02 -0500

    mejora, inserciones, vistas

--- src/main/resources/bd/bd_cetia_v2.sql
+++ src/main/resources/bd/bd_cetia_v2.sql
@@ -26,7 +26,9 @@ on update cascade
 );
 insert into usuario
 value
-(1, "admin", "admin", "cetiaadmin@hotmail.com", null, null, null, null, 2);
+(1, "admin", "admin", "cetiaadmin@hotmail.com", 'Julio Elías', 'Arce Huaman', 35, 75268945, 2);
+INSERT INTO `bd_cetia_v2`.`usuario` (`idusuario`, `cuenta`, `clave`, `correo`, `nombre`, `apellido`, `edad`, `dni`, `idrol`) VALUES ('2', 'janet', 'janet', 'janet@gmail.com', 'Janet', 'Aquino Galindo', '15', '97826178', '2');
+
 
 create table tipo_publicacion (
 idtipopublicacion int auto_increment primary key not null,
@@ -50,6 +52,9 @@ on update cascade
 insert into psicologo
 value
 (1, "administrador", "/img/psicologo");
+INSERT INTO `bd_cetia_v2`.`psicologo` (`idusuario`, `especialidad`, `imagen`) VALUES ('2', 'Psicologa infantil', '');
+
+
 
 create table publicacion(
 idpublicacion int auto_increment primary key not null,
@@ -83,6 +88,7 @@ titulo varchar(100) not null,
 descripcion varchar(1000) null,
 imagen varchar(100) null,
 precio double not null,
+tipo varchar(50) not null, 
 idusuario int not null,
 constraint fk_servicio_psicologo foreign key (idusuario) references psicologo(idusuario)
 on delete cascade
@@ -103,6 +109,38 @@ on delete cascade
 on update cascade
 );
 
+-- vistas de login --
 create view login(user_id, user_cuenta, user_clave, user_rol)
 as
 select idusuario, cuenta, clave, idrol from usuario;
+
+-- vistas de reservacion --
+create view detallereservacion(idservicio,fecha, hora, precio,servicio,nombreusuario, apellidousuario, dniusuario)
+as 
+select res.idreservacion, res.fecha, res.hora, res.precio, ser.titulo, us.nombre, us.apellido, us.dni
+from reservacion as res
+inner join servicio ser 
+on ser.idservicio = res.idservicio
+inner join usuario as us
+on us.idusuario = res.idusuario;
+
+-- vistas publicacion  --
+
+
+-- vista de psicologos--
+create view nuestro_equipo(nombre, apellido, especialidad, foto)
+as
+select us.nombre, us.apellido, ps.especialidad, ps.imagen
+from usuario as us
+inner join psicologo as ps
+on ps.idusuario=us.idusuario
+
+-- vista servicio psicologo -- 
+create view 
+
+select * from servicio
+-- vista activid psicologo --
+
+
+
+
