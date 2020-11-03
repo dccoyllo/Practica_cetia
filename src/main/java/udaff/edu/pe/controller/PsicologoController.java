158a73886fc89706e7e2f236e574c3d4e6ac8256
david ccoyllo <44007590+dccoyllo@users.noreply.github.com>
2020-11-02 20:50:22 -0500

    perfil funcionando parte 01

--- src/main/java/udaff/edu/pe/controller/PsicologoController.java
+++ src/main/java/udaff/edu/pe/controller/PsicologoController.java
@@ -1,16 +1,23 @@
 package udaff.edu.pe.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import udaff.edu.pe.entities.Usuario;
 
 @Controller
 @RequestMapping("/p")
 public class PsicologoController {
 	
 	@GetMapping("/tus-post")
-	public String createPost() {
-		
+	public String createPost(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		 Usuario user = (Usuario) session.getAttribute("user");
+		 
 		return "private/psicologo/tusPost";
 	}
 }
